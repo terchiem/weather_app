0d6570a22c8c39114f03514ca23fc953d5434ef5
Terry Chiem <terrychiem@gmail.com>
2019-08-01 17:44:34 -0700

    add forecast data slotting and temp conversion

---
+++ src/forecast-helpers.js
@@ -0,0 +1,21 @@
+export function separateDays(data) {
+  const dayList = [];
+  for(let i = 0; i < data.list.length; i+=8) {
+    dayList.push(data.list.slice(i, i+8));
+  }
+  return dayList;
+}
+
+export function createDayObject(dayData) {
+  const temps = dayData.map(section => parseFloat(section.main.temp));
+  return {
+    date: Date(dayData[4].dt),
+    high: Math.max(...temps),
+    low: Math.min(...temps),
+    condition: dayData[4].weather[0].main
+  }
+}
+
+export function fToC(temp) {
+  return Math.round((temp - 32)*(5/9));
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import countries from './country-codes';
 import forecast from './test-forecast';
+import { separateDays, createDayObject, fToC } from './forecast-helpers';
 
 Vue.component('search-form', {
   props: ['countries'],
@@ -12,6 +13,7 @@ Vue.component('search-form', {
         <option v-for="(country, key, i) in countries" :value="key">{{country}}</option>
       </select>
       <button @click.prevent="onSubmit">Search</button>
+
       <label>
         F
         <input 
@@ -50,41 +52,80 @@ Vue.component('search-form', {
       this.$emit('search-city', formData)
     },
     toggleTemp() {
-      console.log(this.unit);
+      this.$emit('toggle-temp', this.fahrenheit);
     }
   }
 })
 
 Vue.component('main-card', {
+  props: ['data', 'fahrenheit', 'location'],
   template: `
     <div class="main-card">
-      <h2>Location</h2>
-      <p>Average Temp</p>
-
+      <h2>{{location}}</h2>
       <p>Tomorrow</p>
-      <h3>Day of the week</h3>
-      <p>Condition</p>
+      <h3>{{data.date}}</h3>
+      <p>{{data.condition}}</p>
       <div class="temp-range">
-        <p class="high">High </p>
-        <p class="low">Low</p>
+        <p class="high">{{high}} </p>
+        <p class="low">{{low}}</p>
       </div>
     </div>
-  `
+  `,
+  computed: {
+    high() {
+      let temp = Math.round(this.data.high);
+      let unit = 'F';
+      if(!this.fahrenheit) {
+        temp = fToC(temp);
+        unit = 'C';
+      }
+      return `${temp} ${unit}`
+    },
+    low() {
+      let temp = Math.round(this.data.low);
+      let unit = 'F';
+      if(!this.fahrenheit) {
+        temp = fToC(temp);
+        unit = 'C';
+      }
+      return `${temp} ${unit}`
+    }
+  }
 })
 
 
 Vue.component('card', {
+  props: ['data', 'fahrenheit'],
   template: `
   <div class="card">
-    <h3>Day of the week</h3>
-    <p>Condition</p>
-    <p>Average Temp</p>
+    <h3>{{data.date}}</h3>
+    <p>{{data.condition}}</p>
     <div class="temp-range">
-      <p class="high">High </p>
-      <p class="low">Low</p>
+      <p class="high">{{high}} </p>
+      <p class="low">{{low}}</p>
     </div>
   </div>
-  `
+  `,
+  computed: {
+    high() {
+      let temp = Math.round(this.data.high);
+      let unit = 'F';
+      if(!this.fahrenheit) {
+        temp = fToC(temp);
+        unit = 'C';
+      }
+      return `${temp} ${unit}`
+    },
+    low() {
+      let temp = Math.round(this.data.low);
+      let unit = 'F';
+      if(!this.fahrenheit) {
+        temp = fToC(temp);
+        unit = 'C';
+      }
+      return `${temp} ${unit}`
+    }
+  }
 })
 
 var app = new Vue({
@@ -92,18 +133,54 @@ var app = new Vue({
 
   data: {
     countryCodes: countries,
-    
+    mainData: null,
+    forecastData: null,
+    location: null,
+    fahrenheit: true
   },
 
   methods: {
-    searchCity(data) {
-      console.log(data);
+    toggleTemp(state) {
+      console.log(`temp changed to: ${state}`);
+      this.fahrenheit = state;
     },
+
+    searchCity(data) {
+      this.location = `${data.city}, ${data.country}`;
+      const url = this.formatUrl(data);
+      // fetch data from url
+      // parse jsondata
+      // set data with forecast data
+      console.log(url);
+    },
+
+    formatUrl(data) {
+      const appid = '930555d4bed3616ed708a51ed33135e9';
+      return encodeURI(`http://api.openweathermap.org/data/2.5/forecast?q=${data.city},${data.country}&units=imperial&APPID=${appid}`)
+    },
+
     fetchData(url) {
       //get json from fetch
+
     },
+
     parseData(data) {
+      const days = separateDays(data);
+      const forecastData = days.map(createDayObject);
+      return forecastData;
+    },
+
+    insertData(data) {
+      const forecastData = [...data];
+      this.mainData = forecastData.shift();
+      this.forecastData = forecastData;
+    },
+
+    loadForecast() {
+      const forecastData = this.parseData(forecast);
+      this.insertData(forecastData);
     }
   }
-
 })
+
+app.loadForecast();
